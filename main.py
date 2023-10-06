0edd2aa7a0f40d4fa38822098f8a6e656fd86f3b
mjigormj <igorcruzsilva17@gmail.com>
2023-10-05 22:09:04 -0300

    Alterações Furlan

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-
-#Cria o grafo não direcionado
+# Cria o grafo não direcionado
 G = nx.Graph()
 
-#Adiciona os nós
+# Adiciona os nós
 G.add_node("A")
 G.add_node("B")
 G.add_node("C")
@@ -24,8 +23,7 @@ G.add_node("O")
 G.add_node("P")
 G.add_node("Q")
 
-
-#Adiciona valor nas arestas
+# Adiciona valor nas arestas
 G.add_edge("A", "B", weight=168)
 G.add_edge("B", "C", weight=110)
 G.add_edge("B", "D", weight=265)
@@ -47,45 +45,62 @@ G.add_edge("N", "M", weight=300)
 G.add_edge("M", "P", weight=235)
 G.add_edge("P", "Q", weight=435)
 
-#Acesso dos nós e das arestas
+# Acesso dos nós e das arestas
 nodes = G.nodes()
 edges = G.edges(data=True)
 
-#Atribuindo posição exata dos nós
-node_positions = {"A": (0, 25), "B": (0.180, 24), "C": (0.160, 20), "D": (0.420, 25), "E":
-    (0.430, 21), "F": (0.450, 19), "G": (0.360, 16), "H": (0.250, 14.5), "I": (0.150, 13), "J":
-    (0.153, 10), "K": (0.470, 11), "L": (0.500, 16), "M": (0.400, 1.8), "N": (0.240, -2), "O":
-    (0.170, 0.4), "P": (0.460, 5.1), "Q": (0.700, 2.38)}
+# Aqui que eu tenho que aprender 100% (POSIÇÃO DOS NÓS NO GRAFO)
+node_positions = {"A": (0, 25), "B": (0.180, 24), "C": (0.160, 20), "D": (0.420, 25), "E": (0.430, 21), "F": (0.450, 19), "G": (0.360, 16), "H": (0.250, 14.5), "I": (0.150, 13), "J": (0.153, 10), "K": (0.470, 11), "L": (0.500, 16), "M": (0.400, 1.8), "N": (0.240, -2), "O": (0.170, 0.4), "P": (0.460, 5.1), "Q": (0.700, 2.38)}
 
-#Implementação do algoritmo de Dijkstra
+# Implementação do algoritmo de Dijkstra
 def dijkstra(graph, start, end):
     shortest_path = nx.shortest_path(graph, source=start, target=end, weight='weight')
     shortest_distance = nx.shortest_path_length(graph, source=start, target=end, weight='weight')
     return shortest_path, shortest_distance
 
-#Nó de origem e Nó de destino
-start_node = "I"
-end_node = "Q"
-#Objetivo -> Encontrar o menor entre dois pontos no end_node
+start_node = input("Onde você se encontra neste exato momento ? ")
 
-#Dijkstra
-shortest_path, shortest_distance = dijkstra(G, start_node, end_node)
+# Função para calcular o caminho mais curto
+def shortest_path(graph, start_node, end_node):
+    shortest_path, shortest_distance = dijkstra(graph, start_node, end_node)
+    return shortest_path, shortest_distance
 
-print(f"Caminho mais curto: {shortest_path}")
-print(f"Distância mais curta: {shortest_distance}")
+# Calcular o caminho mais curto de I para Q
+shortest_path_to_Q, shortest_distance_to_Q = shortest_path(G, start_node, "Q")
+print(f"Caminho mais curto de {start_node} para Q: {shortest_path_to_Q}")
+print(f"Distância mais curta de {start_node} para Q: {shortest_distance_to_Q}")
 
+# Calcular o caminho mais curto de I para A
+shortest_path_to_A, shortest_distance_to_A = shortest_path(G, start_node, "A")
+print(f"Caminho mais curto de {start_node} para A: {shortest_path_to_A}")
+print(f"Distância mais curta de {start_node} para A: {shortest_distance_to_A}")
 
-#Nós
+# Comparar os resultados
+if shortest_distance_to_Q < shortest_distance_to_A:
+    print(f"O caminho mais curto é de {start_node} para Q.")
+elif shortest_distance_to_A < shortest_distance_to_Q:
+    print(f"O caminho mais curto é de {start_node} para A.")
+else:
+    print("Ambos os caminhos têm a mesma distância mais curta.")
+
+# Obter o caminho mais curto
+caminho_vermelho, _ = shortest_path(G, start_node, "Q")
+caminho_roxo, _ = shortest_path(G, start_node, "A")
+
+# Nós
 nx.draw(G, pos=node_positions, with_labels=True, node_size=500, node_color="skyblue")
 
-#Arestas
+# Arestas
 labels = nx.get_edge_attributes(G, "weight")
 nx.draw_networkx_edge_labels(G, pos=node_positions, edge_labels=labels)
-nx.draw_networkx_edges(G, pos=node_positions, edgelist=G.edges(), width=0.5, edge_color="gray")
+nx.draw_networkx_edges(G, pos=node_positions, edgelist=G.edges(), width=2, edge_color="gray")
 
-#Caminho mais curto no gráfico
-path_edges = [(shortest_path[i], shortest_path[i + 1]) for i in range(len(shortest_path) - 1)]
+# Caminho mais curto no gráfico
+path_edges = [(caminho_vermelho[i], caminho_vermelho[i + 1]) for i in range(len(caminho_vermelho) - 1)]
 nx.draw_networkx_edges(G, pos=node_positions, edgelist=path_edges, width=2, edge_color="red")
 
-#Mostra o Grafo
+path_edges = [(caminho_roxo[i], caminho_roxo[i + 1]) for i in range(len(caminho_roxo) - 1)]
+nx.draw_networkx_edges(G, pos=node_positions, edgelist=path_edges, width=2, edge_color="purple")
+
+# Mostra o Grafo
 plt.show()
